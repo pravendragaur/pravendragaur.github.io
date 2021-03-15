6dd99f1c2600c3037ea53dc8fc4b9c93afd51da7
pravendragaur <63239703+pravendragaur@users.noreply.github.com>
2021-03-15 07:17:07 +0530

    Update drag&drop.js

--- drag&drop.js
+++ drag&drop.js
@@ -13,8 +13,11 @@ function drop(event){
      event.preventDefault();
      var data = event.dataTransfer.getData("Text");
      var parent = event.target.parentElement;
-     
-    
+
      parent.appendChild(document.getElementById(data));
-     
+
+     if(event.target.id == "to_do_list" || event.target.id == "doing_list" || event.target.id == "done_list"){
+         event.target.appendChild(document.getElementById(data))
+         
+     }
 }
